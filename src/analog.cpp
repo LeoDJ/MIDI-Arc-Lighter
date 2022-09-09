1404b3352795428693d7b00d50896409c62c6821
LeoDJ <leandro.spaeth@gmail.com>
2022-09-09 03:27:34 +0200

    implement UVP and OTP

--- src/analog.cpp
+++ src/analog.cpp
@@ -25,8 +25,7 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc_p) {
 
 void HAL_ADC_ErrorCallback(ADC_HandleTypeDef *hadc) {
     printf("ADC Error 0x%08lX (%ld)\n", hadc->ErrorCode, hadc->ErrorCode);
-    
-    HAL_GPIO_WritePin(LED_1_R_GPIO_Port, LED_1_R_Pin, GPIO_PIN_SET);
+    // HAL_GPIO_WritePin(LED_1_R_GPIO_Port, LED_1_R_Pin, GPIO_PIN_SET);
 }
 
 void analogInit() {
@@ -64,13 +63,42 @@ void analogLoop() {
         uint32_t vdda = VREFINT_VOLTAGE * (*VREFINT_CAL) / avgAdcSamples[CHAN_VREF];               // calculate MCU supply voltage in mV
         uint32_t vbat = avgAdcSamples[CHAN_VBAT] * VIN_DIVIDER_RATIO * vdda / 4095;                // calculate battery voltage in mV
 
-        //See RM0091 section 13.9
+        // Calculate temperature. See RM0091 section 13.9
         int32_t temperature = ((avgAdcSamples[CHAN_TEMP_INT] * vdda) / VREFINT_VOLTAGE) - (int32_t) *TEMP30_CAL_ADDR;
         temperature *= (int32_t)(110000 - 30000);
         temperature = temperature / (int32_t)(*TEMP110_CAL_ADDR - *TEMP30_CAL_ADDR);
         temperature += 30000;
 
-        printf("%4d, %4d, %4d, %4d, %4d\n", avgAdcSamples[0], avgAdcSamples[1], avgAdcSamples[2], avgAdcSamples[3], avgAdcSamples[4]);
-        printf("cal: %4d, vrefint: %4d, vdda: %4d, vbat: %4d, temp: %6d\n", (*VREFINT_CAL), avgAdcSamples[CHAN_VREF], vdda, vbat, temperature);
+        // printf("%4d, %4d, %4d, %4d, %4d\n", avgAdcSamples[0], avgAdcSamples[1], avgAdcSamples[2], avgAdcSamples[3], avgAdcSamples[4]);
+        // printf("cal: %4d, vrefint: %4d, vdda: %4d, vbat: %4d, temp: %6d\n", (*VREFINT_CAL), avgAdcSamples[CHAN_VREF], vdda, vbat, temperature);
+
+        // check for undervoltage
+        if (vbat < BATTERY_CRITICAL_THRESHHOLD && HAL_GetTick() > ANALOG_INIT_TIME) {
+            printf("UNDEVOLTAGE DETECTED! (%d mV) SHUTTING DOWN!\n", vbat);
+
+            // set leds to red
+            HAL_GPIO_WritePin(GPIOB, LED_1_G_Pin | LED_1_B_Pin | LED_2_G_Pin | LED_2_B_Pin, GPIO_PIN_SET); // all LEDs are on port B, TODO: build helper func
+            HAL_GPIO_WritePin(GPIOB, LED_1_R_Pin | LED_2_R_Pin, GPIO_PIN_RESET);
+
+            HAL_Delay(500);
+            
+            // shutdown STM32
+            HAL_PWR_EnterSTANDBYMode();
+        }
+        // check for over temperature
+        if (temperature > OVERTEMPERATURE_THRESHHOLD && HAL_GetTick() > ANALOG_INIT_TIME) {
+            printf("OVERTEMPERATURE DETECTED! (%ld m°C) SHUTTING DOWN!\n", temperature);
+
+            // set leds to orange
+            HAL_GPIO_WritePin(GPIOB, LED_1_R_Pin | LED_1_G_Pin | LED_2_R_Pin | LED_2_G_Pin, GPIO_PIN_RESET); // all LEDs are on port B, TODO: build helper func
+            HAL_GPIO_WritePin(GPIOB, LED_1_B_Pin | LED_2_B_Pin, GPIO_PIN_SET);
+
+            HAL_Delay(500);
+            
+            // shutdown STM32
+            HAL_PWR_EnterSTANDBYMode();
+        }
+
+        printf("%8ld System State - Bat: %4dmV, int. temp.: %d.%d C\n", HAL_GetTick(), vbat, temperature / 1000, temperature % 1000 / 100);
     }
 }
--- src/analog.h
+++ src/analog.h
@@ -2,11 +2,13 @@
 
 #include <stdint.h>
 
+#define BATTERY_CRITICAL_THRESHHOLD 3000    // mV, battery voltage limit under which everything goes into complete shutdown, to draw as little power as possible
+#define OVERTEMPERATURE_THRESHHOLD  80000   // m°C, internal STM temperature limit for complete shutdown
 #define ANALOG_SAMPLE_RATE          100     // ms
 #define VIN_DIVIDER_RATIO           2       // ratio of the supply voltage resistor divider
 #define AVERAGING_FACTOR            100     // 1/x moving average
 #define VREFINT_VOLTAGE             3300    // mV, internal reference voltage of the STM32F103
-#define ANALOG_SENSORS_INIT_TIME    1000    // ms, time after boot until data is valid
+#define ANALOG_INIT_TIME            2000    // ms, time after boot until data is valid
 
 #define VREFINT_CAL_ADDR            0x1FFFF7BA  // address of calibration value, datasheet p. 19
 #define VREFINT_CAL                 ((uint16_t*) VREFINT_CAL_ADDR)
